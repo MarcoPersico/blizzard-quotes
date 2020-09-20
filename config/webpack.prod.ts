db39a661d99b8f319d3bcc2251096e1033fe81cc
Marco Persico <mapersico@deloitte.com>
2020-09-20 06:19:47 -0300

    Building stuff

---
+++ config/webpack.prod.ts
@@ -0,0 +1,40 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+
+const htmlPlugin: HtmlWebPackPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+const envPlugin = new webpack.EnvironmentPlugin({ NODE_ENV: 'production' });
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, '..', 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      {
+        test: /\.(eot|svg|ttf|webp|ico)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'static/',
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin, envPlugin],
+};
+
+export default config;

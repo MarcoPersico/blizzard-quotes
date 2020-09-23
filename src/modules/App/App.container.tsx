944e20ae0b7081c2a19e0dfe736d609e593febbf
Marco Persico <mapersico@deloitte.com>
2020-09-23 18:27:34 -0300

    Added lazy to routes instead of styles

--- src/modules/App/App.container.tsx
+++ src/modules/App/App.container.tsx
@@ -1,23 +1,23 @@
-import loadable from '@loadable/component';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import loadable from '@loadable/component';
 import Animations from '../../assests/Animations.styles';
 import Fonts from '../../assests/Fonts.styles';
 import Navigation from '../Navigation';
-import SectionRouting from '../Routing/Section.routes';
+import GlobalStyles from '../../assests/Global.styles';
 
-const LazyGlobalStyles = loadable(() => import('../../assests/Global.styles'));
+const LazyRoutes = loadable(() => import('../Routing/Section.routes'));
 
 const AppContainer = () => {
   const location = useLocation();
 
   return (
     <>
-      <LazyGlobalStyles currentSection={parseInt(location.pathname.split('/')[2], 10)} />
+      <GlobalStyles currentSection={parseInt(location.pathname.split('/')[2], 10)} />
       <Fonts />
       <Animations />
       <Navigation />
-      <SectionRouting />
+      <LazyRoutes />
     </>
   );
 };
